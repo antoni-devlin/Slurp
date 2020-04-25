6df4321a53b9aaf1cbaa66967c6c09b2921a9334
antoni-devlin <antoni.devlin@gmail.com>
2020-04-25 20:00:04 +0100

    Started setting up basic urls and views

--- Slurp-project/Slurp/urls.py
+++ Slurp-project/Slurp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('recipe/<str:slug>', views.recipe_detail, name='recipe_detail')
 ]
--- Slurp-project/recipes/views.py
+++ Slurp-project/recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+   return render(request, "recipes/home.html")
+
+def recipe_detail(request, slug=slug):
+    slug = "slug"
+    return render(request, "recipes/recipe_detail.html", {'slug':slug})

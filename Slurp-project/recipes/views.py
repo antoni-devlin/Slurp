ff37ecfc239cb0857ce0827ff4a4a45f867ad159
antoni-devlin <antoni.devlin@gmail.com>
2020-05-10 16:40:46 +0100

    Plugged in designed templates

--- Slurp-project/recipes/views.py
+++ Slurp-project/recipes/views.py
@@ -5,7 +5,8 @@ from .forms import RecipeForm
 
 def home(request):
     recipes = Recipe.objects
-    return render(request, "recipes/home.html", {'recipes':recipes})
+    page_title = 'Home'
+    return render(request, "recipes/home.html", {'recipes':recipes, 'page_title':page_title})
 
 def recipe_detail(request, recipe_id):
     recipe_detail = get_object_or_404(Recipe, pk=recipe_id)
